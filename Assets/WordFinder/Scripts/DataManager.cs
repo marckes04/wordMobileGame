798acb92737f090476e238a85bce223120b4b616
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-10-01 11:25:06 -0500

    Data Manager Code

---
+++ Assets/WordFinder/Scripts/DataManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+
+
+    [Header("Data")]
+    private int coins;
+    private int score;
+    private int bestScore;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+        LoadData();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCoins(int amount)
+    {
+        coins += amount;
+        SaveData();
+    }
+
+    public void RemoveCoins(int amount) 
+    { 
+        coins -= amount;
+        coins = Mathf.Max(coins, 0);
+        SaveData();
+    }
+
+    public void IncreaseScore(int amount) 
+    { 
+        score += amount;
+
+        if(score > bestScore)
+            bestScore = score;
+
+        SaveData();
+    }
+
+    public int GetCoins()
+    {
+        return coins;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public int GetBestScore()
+    {
+        return bestScore;
+    }
+
+    private void LoadData()
+    {
+        coins = PlayerPrefs.GetInt("coins", 150);
+        score = PlayerPrefs.GetInt("score");
+        bestScore = PlayerPrefs.GetInt("bestScore");
+    }
+
+
+    private void SaveData()
+    {
+        PlayerPrefs.SetInt("coins", coins);
+        PlayerPrefs.SetInt("score",score);
+        PlayerPrefs.GetInt("bestScore",bestScore);
+    }
+}
